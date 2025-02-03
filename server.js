73ac96def1aaefb4c248edc5a21d9f87e310a7f5
rajbhai129 <hemrajlalawat52@gmail.com>
2025-02-03 21:04:44 +0530

    pool defined

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const session = require("express-session")
 
 const app = express()
 const port = process.env.PORT || 3000
-
+const { Pool } = require('pg');
 // Database connection
 const pool = new Pool({
   user: process.env.DB_USER,
